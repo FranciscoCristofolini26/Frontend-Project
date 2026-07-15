8d86c21825821c56fd8cdc36930624ed0ed1fafc
FranciscoCristofolini26 <francisco.minatti.cristofolini@gmail.com>
2026-07-15 19:37:01 -0300

    base implementation

--- src/app/features/notes/notes.ts
+++ src/app/features/notes/notes.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { Nota } from './models';
 
 @Component({
   selector: 'app-notes',
   imports: [],
   templateUrl: './notes.html',
-  styleUrl: './notes.css',
+  styleUrls: ['./notes.css'],
 })
-export class Notes {}
+export class Notes {
+  notas: Nota[] = [
+      {
+        title: 'Nota 1',
+        content: 'larp da nota 1',
+        edited: new Date(),
+        favourite: false,
+        fixed: false
+      },
+      {
+        title: 'Nota 2',
+        content: 'larp da nota 2',
+        edited: new Date(),
+        favourite: false,
+        fixed: false
+      }
+    ];
+  }
