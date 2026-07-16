24f2fb53dffd79837b14d61ec987caff9193eefb
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-07-15 22:15:46 -0300

    Side bar (#7)
    * Configuração inicial do CSS
    
    * Update app.routes.ts
    
    * Atualização SideBar
    
    ---------
    
    Co-authored-by: Francisco Cristofolini <98647244+FranciscoCristofolini26@users.noreply.github.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Sidebar, Header, Footer } from './shared';
+import { Sidebar, Header, Footer } from './styles/layout';
 import { Calendar, Files, Goals, Habits, Home, Login, Notes, Projects } from './features';
+import { MainLayout } from "./styles/layout/main-layout/main-layout";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, Header, Footer, Calendar, Files, Goals, Habits, Home, Notes, Projects, Login],
+  imports: [RouterOutlet, Sidebar, Header, Footer, Calendar, Files, Goals, Habits, Home, Notes, Projects, Login, MainLayout],
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.css',
